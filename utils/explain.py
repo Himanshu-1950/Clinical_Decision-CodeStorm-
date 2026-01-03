ccbf79fa1620ecb67bb143ea21d622ce7fdadd5d
mehandbisen <bisenmu@rknec.edu>
2026-01-04 01:16:28 +0530

    Initial commit: Clinical Decision Support System implementation

---
+++ utils/explain.py
@@ -0,0 +1,6 @@
+def explain(model, features):
+    return sorted(
+        zip(features, model.feature_importances_),
+        key=lambda x: x[1],
+        reverse=True
+    )
---
+++ utils/recommend.py
@@ -0,0 +1,8 @@
+def recommend(disease):
+    data = {
+        "Diabetes": ["HbA1c","Fasting Sugar"],
+        "Heart Disease": ["ECG","Lipid Profile"],
+        "Flu": ["CBC"],
+        "Cold": []
+    }
+    return data.get(disease, [])
